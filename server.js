7417613ebd7eb66cced92cdcb8d75b846767fb29
sarahduv <sarah.duvdevani@gmail.com>
2019-09-01 10:07:11 -0700

    skeleton working

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// Application Dependencies
+const express = require('express');
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Express middleware
+// Utilize ExpressJS functionality to parse the body of the request
+app.use(express.urlencoded({ extended: true }));
+// Specify a directory for static resources
+app.use(express.static('./public'));
+
+// Set the view engine for server-side templating
+app.set('view engine', 'ejs');
+
+// listen for requests
+app.listen(PORT, () => console.log('Listening on port:', PORT));
+
+// API Routes
+app.get('/', (request, response) => {
+  response.render('./index');
+});
